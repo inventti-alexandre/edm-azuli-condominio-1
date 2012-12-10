9be77986c37cf14b0e3bb94421da9888bf1a08a3
edmls2008@gmail.com <edmls2008@gmail.com@a7636446-2d34-58d4-0e2b-e74c8118fc34>
2012-12-10 12:56:41 +0000

    Adicionando as classes Lancamento Ocorrencia / Ocorrencia

---
+++ Azuli/Azuli.Web.Portal/Azuli.Web.Model/LancamentoOcorrenciaModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Azuli.Web.Model
+{
+    class LancamentoOcorrenciaModel
+    {
+
+        public string ocorrenciaLancamento {get;set;}
+        public DateTime dataOcorrencia { get; set; }
+        public string statusOcorrencia { get; set; }
+        public string descricaoOcorrencia { get; set; }
+        public ApartamentoModel oAp { get; set; }
+        public int codigoOcorrencia { get; set; }
+        public DateTime dataFinalizacao { get; set; }
+        public DateTime dataCancelamento { get; set; }
+
+
+    }
+
+    public class listaLancamento : List<LancamentoOcorrenciaModel> { }
+  
+}
---
+++ Azuli/Azuli.Web.Portal/Azuli.Web.Model/OcorrenciaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Azuli.Web.Model
+{
+    public class OcorrenciaModel
+    {
+        public LancamentoOcorrenciaModel oLancamentoOcorrencia { get; set; }
+        public string descricaoOcorrencia { get; set; }
+        public string statusOcorrencia { get; set;}
+        public DateTime dataCadastroOcorrencia { get; set; }
+
+    }
+
+    public class listaOcorrencia : List<OcorrenciaModel> { }
+}
