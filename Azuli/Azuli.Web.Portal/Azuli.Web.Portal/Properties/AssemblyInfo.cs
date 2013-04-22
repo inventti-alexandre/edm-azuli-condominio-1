65405053b37c8e77fede932694ee99560275a68b
leandrolvilela@gmail.com <leandrolvilela@gmail.com@a7636446-2d34-58d4-0e2b-e74c8118fc34>
2013-04-22 15:00:12 +0000

    Atualização do idioma e nome da compania

--- Azuli/Azuli.Web.Portal/Azuli.Web.Portal/Properties/AssemblyInfo.cs
+++ Azuli/Azuli.Web.Portal/Azuli.Web.Portal/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Azuli.Web.Portal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("FernandesVilela")]
 [assembly: AssemblyProduct("Azuli.Web.Portal")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("pt-BR")]
